07f7788adc47693fa31d333e425c9d210e4b2b13
Lia <liaswapna@gmail.com>
2019-03-07 02:06:19 -0800

    Updates

---
+++ precache-manifest.cf1ab1ed4d04ff03fb048fef4ed09854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a84b0149d85ee08b647dc8f1a21368df",
+    "url": "/clickygame/static/media/bg-tealcloud.a84b0149.jpg"
+  },
+  {
+    "revision": "976821e7c7c83f0311bc",
+    "url": "/clickygame/static/js/runtime~main.3cb4919d.js"
+  },
+  {
+    "revision": "1d00e10271d7ae86a3f9",
+    "url": "/clickygame/static/js/main.7638b29d.chunk.js"
+  },
+  {
+    "revision": "d60d45ec1d55538c25e4",
+    "url": "/clickygame/static/js/2.4f004cb0.chunk.js"
+  },
+  {
+    "revision": "1d00e10271d7ae86a3f9",
+    "url": "/clickygame/static/css/main.4971fb10.chunk.css"
+  },
+  {
+    "revision": "1f1d413301d39c265d06ddcb5fcd7519",
+    "url": "/clickygame/index.html"
+  }
+];
