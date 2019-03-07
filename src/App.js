2208be42795016c9b4aa3d6a85545c30d86dcc43
Lia <liaswapna@gmail.com>
2019-03-06 17:06:18 -0800

    getting the icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDharmachakra,faSnowflake,faCertificate,faAtom,faAsterisk,faSun,faCog,faCompass,faCrosshairs,faHaykal,faPalette,faRing } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <FontAwesomeIcon icon={faDharmachakra} spin/>
+        <FontAwesomeIcon icon={faSnowflake} spin/>
+        <FontAwesomeIcon icon={faCertificate} spin/>
+        <FontAwesomeIcon icon={faAtom} spin/>
+        <FontAwesomeIcon icon={faAsterisk} spin/>
+        <FontAwesomeIcon icon={faSun} spin/>
+        <FontAwesomeIcon icon={faCog} spin/>
+        <FontAwesomeIcon icon={faCompass} spin/>
+        <FontAwesomeIcon icon={faHaykal} spin/>
+        <FontAwesomeIcon icon={faCrosshairs} spin/>
+        <FontAwesomeIcon icon={faPalette} spin/>
+        <FontAwesomeIcon icon={faRing} spin/>
+        
       </div>
     );
   }
